95351835fe05909143b433d91446e87362509d41
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-07-23 20:57:23 +0300

    first stage setup

---
+++ src/components/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Header(props) {
+  return (
+    <div>
+      <h1>DAAY-mall</h1>
+      <nav>
+        <ul>
+          <li>5</li>
+          <li>5</li>
+          <li>5</li>
+          <li>5</li>
+          <li>5</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
