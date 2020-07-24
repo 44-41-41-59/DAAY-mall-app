c0a3826f3ea001f8f613816e99ca41d96cdc0afb
Anolla <annehaddad96@yahoo.com>
2020-07-24 14:32:49 +0300

    almost

---
+++ src/components/footer/index.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+
+
+
+function Footer() {
+  return (
+    <footer>
+
+      <Navbar bg="dark" expand="lg" >
+
+        
+        <div class='container'>
+          <div class='about'>  
+            <h6>About</h6>
+            <p>
+        Lorem ipsum dolor sit amet, vis et tollit ocurreret, eu nec exerci expetenda elaboraret. Per dicit quaestio an. Eos in verear scriptorem.
+            </p>
+          </div>
+          <div class='connect'>
+            <h6>Stay Connected </h6>
+            <p>
+        Lorem ipsum dolor sit amet, vis et tollit ocurreret, eu nec exerci expetenda elaboraret. Per dicit quaestio an. Eos in verear scriptorem.
+            </p>
+          </div>
+          <div class='socialMedia'>
+            <h6>Find us on</h6>
+            <p>
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-google"></a>
+              <a href="#" class="fa fa-yahoo"></a>
+            </p>
+          </div>
+          <div class='help'>
+            <h6>Help Center</h6>
+            <p>
+        Lorem ipsum dolor sit amet, vis et tollit ocurreret, eu nec exerci expetenda elaboraret. Per dicit quaestio an. Eos in verear scriptorem.
+            </p> 
+          </div>
+        </div>
+        
+      </Navbar>
+    </footer>
+    // <footer>
+    //   <div class='container'>
+    //     <div class='about'>  
+    //       <h6>About</h6>
+    //       <p>
+    //       Lorem ipsum dolor sit amet, vis et tollit ocurreret, eu nec exerci expetenda elaboraret. Per dicit quaestio an. Eos in verear scriptorem.
+    //       </p>
+    //     </div>
+    //     <div class='connect'>
+    //       <h6>Stay Connected </h6>
+    //       <p>
+    //       Lorem ipsum dolor sit amet, vis et tollit ocurreret, eu nec exerci expetenda elaboraret. Per dicit quaestio an. Eos in verear scriptorem.
+    //       </p>
+    //     </div>
+    //     <div class='socialMedia'>
+    //       <h6>Find us on</h6>
+    //       <p>
+    //         <a href="#" class="fa fa-facebook"></a>
+    //         <a href="#" class="fa fa-twitter"></a>
+    //         <a href="#" class="fa fa-google"></a>
+    //         <a href="#" class="fa fa-yahoo"></a>
+    //       </p>
+    //     </div>
+    //     <div class='help'>
+    //       <h6>Help Center</h6>
+    //       <p>
+    //       Lorem ipsum dolor sit amet, vis et tollit ocurreret, eu nec exerci expetenda elaboraret. Per dicit quaestio an. Eos in verear scriptorem.
+    //       </p> 
+    //     </div>
+    //   </div>
+    // </footer>
+  );
+}
+  
+export default Footer;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+
 
 export default function Header(props) {
   return (
-    <div>
-      <h1>DAAY-mall</h1>
-      <nav>
-        <ul>
-          <li>5</li>
-          <li>5</li>
-          <li>5</li>
-          <li>5</li>
-          <li>5</li>
-        </ul>
-      </nav>
-    </div>
+    <header>
+      <Navbar bg="light" expand="lg" >
+        
+        <Navbar.Brand href="#home">DAAY-mall</Navbar.Brand>
+
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto" style={{marginRight:'100'}} >
+            <NavDropdown title="User" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Edit Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">My orders</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Favorites</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">Log out</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Cart" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">View Cart</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="Help" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Customer Service</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Return Poilicy</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Shipping Policy</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+         
+        </Navbar.Collapse>
+      </Navbar>
+    </header>
   );
 }
