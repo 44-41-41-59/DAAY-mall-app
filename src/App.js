5aadca7562e76fdcc6337eafc8039b5e8f08186e
Ahmed199764 <Ahmed199764@hotmail.com>
2020-07-24 18:47:55 +0300

    product

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Aside from '../src/components/header';
+// import Aside from '../src/components/header';
 // import { Route } from 'react-router-dom';
+import Products from '../src/components/product/app.js';
 
 function App() {
   return (
     <div className="App">
-      <Aside />
-      <h1>HELLO WORLD!!</h1>
+      {/* <Aside /> */}
+      <Products />
+      {/* <h1>HELLO WORLD!!</h1> */}
     </div>
   );
 }
---
+++ src/components/product/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import BuyProductSlid from './buyProductSlide';
+import BuyProductSided from './buyProductSided';
+import BuyProductSecSlid from './buyProductSecSlide';
+
+function App() {
+    return (
+      <div className="App">
+        <BuyProductSlid />
+        <BuyProductSided />
+        <BuyProductSecSlid />
+      </div>
+    );
+  }
+
+  export default App;
