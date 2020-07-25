fbba6412551d07b834d42f5cd04761bc2fde4a46
YazanAlaiwah <yazanalaiwah@gmail.com>
2020-07-26 00:00:20 +0300

    add border to the auth

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,43 +1,46 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 function Login(props) {
   return (
-    <Form onSubmit={props.handleSubmitLogin}>
-      <h3>LOGIN</h3>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          required
-          type="email"
-          name="email"
-          // value={props.body.email}
-          onChange={props.handelChange}
-          placeholder="Enter email"
-        />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Card body>
+      <Form onSubmit={props.handleSubmitLogin}>
+        <h3>LOGIN</h3>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            type="email"
+            name="email"
+            // value={props.body.email}
+            onChange={props.handelChange}
+            placeholder="Enter email"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          required
-          type="password"
-          onChange={props.handelChange}
-          name="password"
-          placeholder="Password"
-          // value={props.body.password}
-        />
-      </Form.Group>
-      <Form.Group required controlId="formBasicCheckbox">
-        {/* <Form.Check required type="checkbox" label="Check me out" /> */}
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            onChange={props.handelChange}
+            name="password"
+            placeholder="Password"
+            // value={props.body.password}
+          />
+        </Form.Group>
+        <Form.Group required controlId="formBasicCheckbox">
+          {/* <Form.Check required type="checkbox" label="Check me out" /> */}
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Card>
   );
 }
 
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,62 +1,65 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 function Signup(props) {
   return (
-    <Form onSubmit={props.handleSubmit}>
-      <h3>SIGNUP</h3>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          required
-          type="text"
-          name="username"
-          // value={props.body.username}
-          placeholder="Enter ur lovely name"
-          onChange={props.handelChange}
-        />
-        <Form.Text className="text-muted">
-          show us your cool name lady if u r anolla or diana don't kill me pls
-        </Form.Text>
-      </Form.Group>
+    <Card body>
+      <Form onSubmit={props.handleSubmit}>
+        <h3>SIGNUP</h3>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            name="username"
+            // value={props.body.username}
+            placeholder="Enter ur lovely name"
+            onChange={props.handelChange}
+          />
+          <Form.Text className="text-muted">
+            show us your cool name lady if u r anolla or diana don't kill me pls
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group controlId="formBasicUserName">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          onChange={props.handelChange}
-          // value={props.body.email}
-          name="email"
-          required
-          type="email"
-          placeholder="Enter username"
-        />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else. 😇
-        </Form.Text>
-      </Form.Group>
+        <Form.Group controlId="formBasicUserName">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            onChange={props.handelChange}
+            // value={props.body.email}
+            name="email"
+            required
+            type="email"
+            placeholder="Enter username"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else. 😇
+          </Form.Text>
+        </Form.Group>
 
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          name="password"
-          // value={props.body.password}
-          onChange={props.handelChange}
-          required
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check
-          required
-          type="checkbox"
-          label="I read all the roles and understand it"
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
+            // value={props.body.password}
+            onChange={props.handelChange}
+            required
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check
+            required
+            type="checkbox"
+            label="I read all the roles and understand it"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Card>
   );
 }
 
