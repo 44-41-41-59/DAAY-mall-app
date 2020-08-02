0e9b32c6ddafd41d9f50202322ebd6c6bd461a85
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-08-02 14:36:14 +0300

    header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import HeaderNav from './headerNav';
 import { Link, useHistory } from 'react-router-dom';
+import { useSelector, useDispatch, connect } from 'react-redux';
 import {
   MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBDropdown,
   MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBIcon,
 } from 'mdbreact';
-import Show from '../show';
-import { useSelector, useDispatch, connect } from 'react-redux';
-import { logout } from '../../store/actions/auth';
 import { If, Then, Else } from '../if/if';
+import Show from '../show';
 
+import { logout } from '../../store/actions/auth';
 
 import './header.css';
 
@@ -46,7 +42,7 @@ function Header(props) {
               <MDBNavLink to='/'>Home</MDBNavLink>
             </MDBNavItem>
             <MDBNavItem>
-              <MDBNavLink to="#!">Stores</MDBNavLink>
+              <MDBNavLink to='/stores'>Stores</MDBNavLink>
             </MDBNavItem>
             <MDBNavItem>
               <MDBNavLink to='/aboutus'>About us</MDBNavLink>
@@ -63,32 +59,30 @@ function Header(props) {
 
             <MDBNavItem>
               <MDBDropdown>
-                <MDBDropdownToggle nav >
+                <MDBDropdownToggle nav caret >
                   <div className="d-none d-md-inline">
-                    <MDBIcon icon="question-circle" />
+                    <MDBIcon icon="question-circle" style={{ marginRight: '1vw' }} />
                     Help
                   </div>
                 </MDBDropdownToggle>
-                {/* <MDBDropdownMenu className="dropdown-default">
+                <MDBDropdownMenu className="dropdown-default">
                   <MDBDropdownItem >
-                    <Link to=''>Customer Service</Link>
+                    <Link to='/customerservice'>Customer Service</Link>
                   </MDBDropdownItem>
                   <MDBDropdownItem >
-                    <Link to=''>Return Policy</Link>
-                  </MDBDropdownItem>
-                  <MDBDropdownItem >
-                    <Link to=''>Shipping Policy</Link>
+                    <Link to=''>Website Policies</Link>
                   </MDBDropdownItem>
                   <MDBDropdownItem >
                     <Link to=''>FAQ</Link>
                   </MDBDropdownItem>
-                </MDBDropdownMenu> */}
+                </MDBDropdownMenu>
               </MDBDropdown>
             </MDBNavItem>
 
-            <MDBNavItem>
-              <If condition={!logedin}>
-                <Then>
+            <If condition={!logedin}>
+              <Then>
+                <MDBNavItem>
+
                   <MDBDropdownToggle nav >
                     <Link to='/auth' style={{ color: 'white' }}>
                       <div className="d-none d-md-inline">
@@ -97,18 +91,14 @@ function Header(props) {
                       </div>
                     </Link>
                   </MDBDropdownToggle>
-                </Then>
-                <Else>
+                </MDBNavItem>
+              </Then>
+              <Else>
+                <MDBNavItem>
                   <MDBDropdown>
                     <MDBDropdownToggle nav caret>
                       <div className="d-none d-md-inline">
-                        <Link to={`/cart/user/${props.user._id}`} style={{ color: 'white', marginLeft: '1vw' }}>
-                          <MDBIcon icon="shopping-cart" />
-                          Cart ({props.user.cart.length})
-                        </Link>
-                      </div>
-                      <div className="d-none d-md-inline">
-                        <i class="fas fa-user"></i>
+                        <i class="fas fa-user" style={{ marginRight: '1vw' }}></i>
                         Profile
                       </div>
                     </MDBDropdownToggle>
@@ -116,6 +106,11 @@ function Header(props) {
                       <MDBDropdownItem >
                         <Link to={`/profile/${props.user._id}`}>My profile</Link>
                       </MDBDropdownItem>
+                      <Show condition={props.user.role==='owner'}>
+                        <MDBDropdownItem >
+                          <Link to={`/store/${props.user.stores}`}>My store</Link>
+                        </MDBDropdownItem>
+                      </Show>
                       <MDBDropdownItem >
                         <Link to={`/profile/${props.user._id}`}>My orders</Link>
                       </MDBDropdownItem>
@@ -130,115 +125,25 @@ function Header(props) {
                       </MDBDropdownItem>
                     </MDBDropdownMenu>
                   </MDBDropdown>
-                </Else>
-              </If>
-            </MDBNavItem>
+                </MDBNavItem>
+                <MDBNavItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle nav >
+                      <div className="d-none d-md-inline" style={{ marginRight: '1vw' }}>
+                        <Link to={`/cart/user/${props.user._id}`} style={{ color: 'white' }}>
+                          <MDBIcon icon="shopping-cart" style={{ marginRight: '1vw' }} />
+                          Cart {props.user.cart.length}
+                        </Link>
+                      </div>
+                    </MDBDropdownToggle>
+                  </MDBDropdown>
+                </MDBNavItem>
+              </Else>
+            </If>
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBNavbar>
-      <Show condition={props.user.role==='owner'}>
-        <HeaderNav />
-      </Show>
     </>
-
-    // <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
-    //   <Navbar.Brand href="#home">DAAY-mall</Navbar.Brand>
-    //   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    //   <Navbar.Collapse id="responsive-navbar-nav">
-    //     <Nav className="mr-auto">
-    //       <Nav.Link href="#features">Features</Nav.Link>
-    //       <Nav.Link href="#pricing">Pricing</Nav.Link>
-    //       <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-    //         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-    //         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-    //         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-    //         <NavDropdown.Divider />
-    //         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-    //       </NavDropdown>
-    //     </Nav>
-    //     <Nav>
-    //       <Nav.Link href="#deets">More deets</Nav.Link>
-    //       <Nav.Link eventKey={2} href="#memes">
-    //         Dank memes
-    //       </Nav.Link>
-    //     </Nav>
-    //   </Navbar.Collapse>
-    // </Navbar>
-    // <>
-    //   <header>
-    //     <Navbar bg="light" expand="lg" id="header">
-    //       <div id="logoImg">
-    //         <Link to="/">
-    //           <Navbar.Brand>
-    //             <MDBIcon icon="weight-hanging" id="logoIcon" />
-    //           DAAY-mall
-    //           </Navbar.Brand>
-    //         </Link>
-    //       </div>
-
-    //       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //       <Navbar.Collapse id="basic-navbar-nav">
-    //         <div class="form-group has-search" id="searchFormHeader">
-    //           <span
-    //             class="fa fa-search form-control-feedback"
-    //             id="searchIcon"
-    //           ></span>
-    //           <input type="text" class="form-control" placeholder="Search" />
-    //         </div>
-    //         <Nav className="mr-auto">
-    //           <Show condition={!logedin}>
-    //             {' '}
-    //             <Link to="/auth">sign in</Link>{' '}
-    //           </Show>
-    //           <MDBIcon far icon="user" id="userIcon" />
-    //           <NavDropdown id="basic-nav-dropdown">
-    //             <NavDropdown.Item>
-    //               <Link to={`/profile/${props.user._id}`}>Profile</Link>
-    //             </NavDropdown.Item>
-    //             <NavDropdown.Item >My orders</NavDropdown.Item>
-    //             <NavDropdown.Item >Favorites</NavDropdown.Item>
-    //             <NavDropdown.Item>Settings</NavDropdown.Item>
-    //             <Show condition={logedin}>
-    //               <NavDropdown.Item onClick={handelLogout}>
-    //               Log out
-    //               </NavDropdown.Item>
-    //             </Show>
-    //           </NavDropdown>
-
-    //           <p id="numOfitemsAddedToCart">{props.user.cart.length}</p>
-    //           {!props.user._id && (
-    //             <Link to={`/auth`}>
-    //               <div id="cartIcon" style={{ color: 'black' }}>
-    //                 <MDBIcon icon="shopping-cart" />
-    //               </div>
-    //             </Link>
-    //           )}
-    //           {props.user._id && (
-    //             <Link to={`/cart/user/${props.user._id}`}>
-    //               <div id="cartIcon" style={{ color: 'black' }}>
-    //                 <MDBIcon icon="shopping-cart" />
-    //               </div>
-    //             </Link>
-    //           )}
-
-    //           <NavDropdown title="Help" id="basic-nav-dropdown">
-    //             <NavDropdown.Item href="#action/3.1">
-    //             Customer Service
-    //             </NavDropdown.Item>
-    //             <NavDropdown.Item href="#action/3.2">
-    //             Return Poilicy
-    //             </NavDropdown.Item>
-    //             <NavDropdown.Item href="#action/3.3">
-    //             Shipping Policy
-    //             </NavDropdown.Item>
-    //           </NavDropdown>
-    //         </Nav>
-    //       </Navbar.Collapse>
-    //     </Navbar>
-    //   </header>
-    //   <HeaderNav/>
-
-    // </>
   );
 }
 
--- src/service/socket.js
+++ src/service/socket.js
@@ -1,5 +1,6 @@
 import io from 'socket.io-client';
-const ENDPOINT = 'http://localhost:3001';
+// const ENDPOINT = 'http://localhost:3001';
+const ENDPOINT = 'https://daaymall-401-project.herokuapp.com';
 
 export let socket;
 
